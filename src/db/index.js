c567a399486ca1df8628abf35a2ea0f074360806
SuyashN07 <suyashgnaman07@gmail.com>
2025-11-08 16:27:28 +0530

    utisl code created

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async() => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`\nMONGODB CONNECTED !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.error("Error connecting Database", error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import express from 'express';
-import { DB_NAME } from './constants.js';
+import connectDB from './db/index.js';
 
 dotenv.config()
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server running on port:${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Error connecting to database:  ", err);
+})

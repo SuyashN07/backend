ed238d7cf6d6f663ac1b7f9dd7bcd1e699463939
SuyashN07 <suyashgnaman07@gmail.com>
2025-11-13 00:56:13 +0530

    base for routes, controllers added

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.static("public"))
 
 app.use(cookieParser());
 
+//Import routes
+import userRouter from './routes/user.routes.js';
+
+//Use routes
+app.use('/api/v1/users', userRouter);
+
 export { app }

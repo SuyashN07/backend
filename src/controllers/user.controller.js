4660b1201a379aa4521b0af4977a81ef68dc4cc8
SuyashN07 <suyashgnaman07@gmail.com>
2025-11-15 12:46:09 +0530

    bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler( async(req,res) => {
     }
 
     //check already exist
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     });
 
@@ -27,7 +27,12 @@ const registerUser = asyncHandler( async(req,res) => {
 
     //check avatar, image
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(coverImageLocalPath && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
  
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avtar file required");
